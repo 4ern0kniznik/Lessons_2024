200a7d24b56f0526eeda23532c70b92dc061fbb4
yaitskii <xooloru@gmail.com>
2024-05-07 21:06:13 +0300

    Repetition part two

---
+++ src/Repetition/Task_5.java
@@ -0,0 +1,10 @@
+package Repetition;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        int year = 1980;
+        for(int i = year; i<= 2020; i+=4){
+            System.out.println("Олимпиада" + i + " " + "года");
+        }
+    }
+}
