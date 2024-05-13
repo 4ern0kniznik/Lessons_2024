df966cfd00372565e22b8948ad46401806f69d33
Lex Raiden <xooloru@gmail.com>
2024-05-13 23:04:59 +0300

    Examination_1

---
+++ src/Examination/Task_1.java
@@ -0,0 +1,36 @@
+package Examination;
+
+/**
+ * Вернуть число среднее арифметическое всех элементов массива / вернуть в дабл
+ * - public static int average (int [] array)
+ * or
+ * - public static double average (int [] array)
+ */
+public class Task_1 {
+    public static void main(String[] chars) {
+        System.out.println("Среднее арифметическое: ");
+        int[] a = {1,2,3,4};
+        int[] b = {5,6,7,8};
+        System.out.println(average(a) + " " + average(b));
+    }
+    public static double average(int[] array){
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+
+
+        /**
+         *   // Сумма всех элементов массива
+         *         System.out.println("Сумма всех элементов массива : ");
+         *
+         *         int sum = 0;
+         *         for (int i = 0; i < arrayLength; i++) {
+         *             sum += array[i];
+         *         }
+         *         System.out.println(sum);
+         */
+
+    }
+}
